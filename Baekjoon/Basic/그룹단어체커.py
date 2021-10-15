18bfc59ee1d1dda2756e9f474498d62a16b7dc22
unknown <ansxogh97@gmail.com>
2021-10-15 15:12:08 +0900

    20211015

---
+++ Baekjoon/Basic/그룹단어체커.py
@@ -0,0 +1,15 @@
+# def setence_checker(number,sentence):
+#     for i in range(number):
+#         for i in sentence
+
+
+
+
+
+
+
+number = int(input())
+sentence = []
+for i in range(number):
+    sentence.append(input())
+print(sentence)
--- Baekjoon/Basic/크로아티아_알파벳.py
+++ Baekjoon/Basic/크로아티아_알파벳.py
@@ -1,56 +1,15 @@
 def croatia_alpha(x):
-    x = list(x)
     answer = 0
+    alphabet = ['dz=','c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']
     length = len(x)
-    for i in range(2,len(x)):
-        if x[i] == '=':
-            if x[i-1] == 'z':
-                if x[i-2] == 'd':
-                    answer += 1
-                    del x[i]
-                    del x[i-1]
-                    del x[i-2]
-                    length -= 3
-    for i in range(1,length):
-        if x[i] == '=':
-            if x[i-1] =='c':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-            if x[i-1] =='s':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-            if x[i-1] =='z':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-        elif x[i] == 'j':
-            if x[i-1] =='l':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-            if x[i-1] =='n':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-        elif x[i] == '-':
-            if x[i-1] =='c':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-            if x[i-1] =='d':
-                answer += 1
-                del x[i]
-                del x[i-1]
-                length -= 2
-    print(x)
+    for i in alphabet:
+        if i in x:
+            answer += x.count(i)
+            x = x.replace(i,(' '*len(i)))
+            
+    print(answer + (length - x.count(' ')))
+
+
 
 x = input()
 croatia_alpha(x)
