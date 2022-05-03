3fe72afc176a85da479906e9c32cfef91f135856
PandaMun <61276800+Muntaeho@users.noreply.github.com>
2022-05-03 23:10:59 +0900

    20220503

---
+++ Programmers/Hash/위장.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    answer = 1
+    count = len(clothes)
+    type = {}
+    for i in clothes:
+        type[i[1]] = []
+    for i in clothes:
+        type[i[1]].append(i[0])
+    for i in type.keys():
+        answer *= len(type[i]) + 1
+    return answer - 1
