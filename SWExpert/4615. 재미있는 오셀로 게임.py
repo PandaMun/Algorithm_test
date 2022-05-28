71ba94b8a485c5cc936c5e3c27599d78e59b9e26
문태호 <ansxogh97@gmail.com>
2022-05-29 00:51:06 +0900

    20220529

--- SWExpert/4615. 재미있는 오셀로 게임.py
+++ SWExpert/4615. 재미있는 오셀로 게임.py
@@ -2,8 +2,8 @@ answer = []
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    dx = [0,0,1,-1,1,-1]
-    dy = [1,-1,0,0,1,-1]
+    dx = [0,0,1,-1,1,-1,1,-1]
+    dy = [1,-1,0,0,1,-1,-1,1]
     n,m = map(int,input().split())
     matrix = []
     for i in range(n):
@@ -20,22 +20,22 @@ for test_case in range(1, T + 1):
             seq += [0]*n
         matrix.append(seq)
     for i in range(m):
-        x,y,color = map(int,input().split())
+        y,x,color = map(int,input().split())
+        x-=1
+        y-=1
         if color == 1:
             find_color = 2
         else:
             find_color = 1
-        x = y-1
-        y = x-1
         matrix[x][y] = color
-        for i in range(6):
-            nx = x
-            ny = y
+        for i in range(8):
+            nx = 0 + x
+            ny = 0 + y
             find = []
             while(1):
                 nx = nx + dx[i]
                 ny = ny + dy[i]
-                if 0 < x <= n-1 and 0 < y <= n-1:
+                if 0 <= nx <= n-1 and 0 <= ny <= n-1:
                     if matrix[nx][ny] == find_color:
                         find.append([nx,ny])
                     elif matrix[nx][ny] == 0:
@@ -44,10 +44,18 @@ for test_case in range(1, T + 1):
                         if len(find) != 0:
                             for i in find:
                                 matrix[i[0]][i[1]] = color
-                            print(matrix)
                             break
+                        else:
+                            break
+                            
                 else:
                     break
-        print(matrix)
-    print(matrix)
+    black = 0
+    white = 0
+    for i in (matrix):
+        black += i.count(1)
+        white += i.count(2)
+    answer.append(str(black) + ' ' + str(white))
+for i in range(len(answer)):
+    print('#' +str(i+1) + ' ' + str(answer[i]))
 
