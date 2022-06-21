8e1dc370313a2909e1e87623287e59e00b776370
문태호 <ansxogh97@gmail.com>
2022-06-21 23:48:25 +0900

    20220621

---
+++ Baekjoon/Basic/16457 단풍잎 이야기.py
@@ -0,0 +1,23 @@
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+answer = []
+from itertools import permutations
+n,m,k = map(int,input().split())
+quests = []
+total_key = []
+for i in range(m):
+    quest = list(map(int,input().split()))
+    quests.append(quest)
+    total_key += quest
+total_key = list(set(total_key))
+key_combination = list(permutations(total_key,n))
+for i in key_combination:
+    success = 0
+    for j in quests:
+        example = list(i[:])
+        example += j
+        if len(set(example)) == len(i):
+            success += 1
+    answer.append(success)
+print(max(answer))
