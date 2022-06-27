be8b889b48de8acd52d0c5ebf81b935d8e96dd39
문태호 <ansxogh97@gmail.com>
2022-06-27 23:57:10 +0900

    20220627

--- Baekjoon/Basic/11723 집합.py
+++ Baekjoon/Basic/11723 집합.py
@@ -20,7 +20,7 @@ for i in range(int(input())):
         else:
             stack.append(command[1])
     elif command[0] == 'all':
-        stack = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+        stack = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
     elif command[0] == 'empty':
         stack = []
 
--- Baekjoon/Basic/18310 안테나.py
+++ Baekjoon/Basic/18310 안테나.py
@@ -1,9 +1,8 @@
 import sys
-import math
 input = sys.stdin.readline
 
 cnt = int(input())
 house = list(map(int,input().split()))
 
 house.sort()
-print(house[(cnt//2)-1])
+print(house[(cnt-1)//2])
