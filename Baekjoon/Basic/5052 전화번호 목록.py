35c7bd3b7cfc644a8cc7c819f1b949a335c18661
문태호 <ansxogh97@gmail.com>
2022-07-30 23:56:24 +0900

    20220730

--- Baekjoon/Basic/5052 전화번호 목록.py
+++ Baekjoon/Basic/5052 전화번호 목록.py
@@ -1,9 +1,22 @@
 import sys
 input = sys.stdin.readline
 answer = []
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
 for i in range(int(input())):
-    phone = []
+    phone_book = []
     result = ''
     for i in range(int(input())):
-        phone.append(input())
-    for i in phone:
+        phone = input().rstrip()
+        phone_book.append(phone)
+    phone_book.sort()
+    if solution(phone_book):
+        answer.append('YES')
+    else:      
+        answer.append('NO')
+for i in answer:
+    print(i)
