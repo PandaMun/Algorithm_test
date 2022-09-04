ab8a051b1b6e9d1c76a52475e24386b89dd813e7
문태호 <ansxogh97@gmail.com>
2022-09-04 22:41:47 +0900

    20220904

---
+++ Programmers/Java/Level2/StringCompression.java
@@ -0,0 +1,49 @@
+package Level2;
+
+public class StringCompression {
+
+    public static int solution(String s) {
+        int answer = s.length();
+        for (int i = 1; i <= s.length() / 2; i++) {
+            int cnt = 0;
+            int len = 0;
+            boolean finish = true;
+            for (int j = 0; j < s.length() - i; j += i) {
+                if (j + (2 * i) > s.length()) {
+                    len += (s.length() - (j + i));
+                    if (cnt == 0) {
+                        len += i;
+                    }
+                    finish = false;
+                    break;
+                }
+                if (s.substring(j, j + i).equals(s.substring(j + i, j + 2 * i))) {
+                    cnt += 1;
+                } else {
+                    if (cnt > 0) {
+                        len += Integer.toString(cnt).length();
+                        len += i;
+                        cnt = 0;
+                    } else {
+                        len += i;
+                    }
+                }
+            }
+            if (cnt > 0) {
+                len += Integer.toString(cnt).length();
+                len += i;
+            } else {
+                if (finish) {
+                    len += i;
+                }
+            }
+            answer = Math.min(len, answer);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "a";
+        System.out.println(solution(s));
+    }
+}
