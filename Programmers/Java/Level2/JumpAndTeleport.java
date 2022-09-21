3e86ca6e833c5ce0b52df56df8dc3ff179296f07
PandaMun <61276800+PandaMun@users.noreply.github.com>
2022-09-21 16:04:42 +0900

    Update JumpAndTeleport.java

--- Programmers/Java/Level2/JumpAndTeleport.java
+++ Programmers/Java/Level2/JumpAndTeleport.java
@@ -2,17 +2,19 @@ package Level2;
 
 public class JumpAndTeleport {
     public static int solution(int n) {
-        int ans = 0;
+         int ans = 0;
         while (true) {
             if (n == 0) {
-
+                break;
             }
             if (n % 2 == 1) {
                 n -= 1;
+                ans += 1;
             } else {
                 n /= 2;
             }
         }
+        return ans;
     }
 
     public static void main(String[] args) {
