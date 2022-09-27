6d584ca0b9b5a93eec6c4a9fbfd54ef62497fec3
PandaMun <61276800+Muntaeho@users.noreply.github.com>
2022-09-27 23:56:03 +0900

    20220927

---
+++ Baekjoon/Basic/main_10173.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main_10173 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = br.readLine();
+            if (line.equals("EOI")) {
+                break;
+            }
+            if (line.toLowerCase().matches(".*(nemo).*")) {
+                System.out.println("Found");
+            } else {
+                System.out.println("Missing");
+            }
+        }
+    }
+}
