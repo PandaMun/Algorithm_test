8751b907cfb0a48f128183fe226398590db6f3b1
PandaMun <61276800+Muntaeho@users.noreply.github.com>
2023-03-27 23:56:23 +0900

    20230327

---
+++ Baekjoon/Basic/Main_17298.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_17298 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] answer = new int[n];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < a[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) {
+                answer[i] = -1;
+            } else if (!stack.isEmpty()) {
+                answer[i] = stack.peek();
+            }
+            stack.push(a[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(answer[i] + " ");
+        }
+
+    }
+
+}
